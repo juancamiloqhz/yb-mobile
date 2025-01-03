82bf438c098cc34b94c8fea0729d9f8ffa2871c8
JuanCamiloQHz <juancamiloqhz@gmail.com>
2025-01-03 12:26:56 -0500

    adding react-native-reusables with npx @react-native-reusables/cli@latest init and formatting all the files with prettier config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,109 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import "@/global.css"
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import * as React from "react"
+import { Platform, View } from "react-native"
+import {
+  DarkTheme,
+  DefaultTheme,
+  Theme,
+  ThemeProvider,
+} from "@react-navigation/native"
+import { PortalHost } from "@rn-primitives/portal"
+import { useFonts } from "expo-font"
+import { Stack } from "expo-router"
+import * as SplashScreen from "expo-splash-screen"
+import { StatusBar } from "expo-status-bar"
+
+import { setAndroidNavigationBar } from "@/lib/android-navigation-bar"
+import config from "@/lib/config"
+import { NAV_THEME } from "@/lib/constants"
+import { Bell } from "@/lib/icons/Bell"
+import { UserCircle } from "@/lib/icons/UserCircle"
+import { useColorScheme } from "@/lib/useColorScheme"
+import { ThemeToggle } from "@/components/ThemeToggle"
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+}
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+}
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router"
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const hasMounted = React.useRef(false)
+  const { colorScheme, isDarkColorScheme } = useColorScheme()
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  })
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false)
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current || !loaded) {
+      return
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
+    if (Platform.OS === "web") {
+      // Adds the background color to the html element to prevent white background on overscroll.
+      document.documentElement.classList.add("bg-background")
+    }
+    setAndroidNavigationBar(colorScheme)
+    setIsColorSchemeLoaded(true)
+    hasMounted.current = true
+    SplashScreen.hideAsync()
+  }, [loaded])
+
+  if (!isColorSchemeLoaded || !loaded) {
+    return null
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
+        <Stack.Screen
+          name="index"
+          options={{
+            title: config.name,
+            headerLeft: () => (
+              <>
+                <UserCircle
+                  className="text-foreground"
+                  size={23}
+                  strokeWidth={1.25}
+                />
+              </>
+            ),
+            headerRight: () => (
+              <View className="flex-row items-center gap-3">
+                <Bell
+                  className="text-foreground"
+                  size={23}
+                  strokeWidth={1.25}
+                />
+                <ThemeToggle />
+              </View>
+            ),
+          }}
+        />
       </Stack>
-      <StatusBar style="auto" />
+      <PortalHost />
     </ThemeProvider>
-  );
+  )
 }
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === "web" && typeof window === "undefined"
+    ? React.useEffect
+    : React.useLayoutEffect
---
+++ lib/config.ts
@@ -0,0 +1,9 @@
+type Config = {
+  name: string
+}
+
+const config = {
+  name: "YellowBird",
+} satisfies Config
+
+export default config
